c631d135c3d64c1990d07aabdb8bb785b98fe20d
Muaaz Bin Saeed <muaaz.binsaeed@hotmail.com>
2024-04-27 05:38:31 +0500

    commnds

---
+++ commands.sh
@@ -0,0 +1,33 @@
+npm install && npm run dev
+
+
+npm install react-collapsible --save
+
+npm install rc-collapse 
+
+npm i bootstrap
+
+npm i react-bootstrap
+
+
+# -------
+npm install
+npm run dev
+
+
+
+
+--------
+
+
+git init
+
+npm i 
+npm run dev
+
+
+git init 
+git add .
+git commit -m "Initial commit of my React project"
+git remote add origin https://github.com/Muaazbinsaeed/MedicalClinicalTrial_ITable.git
+git push -u origin main
